8b7ef9ee5248098d0ae9a55c4ecff036b1789fe6
Phelipe Pereira <phelipe.psouza@senacsp.edu.br>
2024-03-01 15:56:06 -0300

    Adicionar arquivos de projeto.

---
+++ MagicTrickGame/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace MagicTrickGame
+{
+    partial class MagicTrick
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MagicTrick));
+            this.btnNewGame = new System.Windows.Forms.Button();
+            this.btnRules = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnNewGame
+            // 
+            this.btnNewGame.Font = new System.Drawing.Font("Arial", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnNewGame.Location = new System.Drawing.Point(398, 273);
+            this.btnNewGame.Name = "btnNewGame";
+            this.btnNewGame.Size = new System.Drawing.Size(222, 70);
+            this.btnNewGame.TabIndex = 0;
+            this.btnNewGame.Text = "Criar Partida";
+            this.btnNewGame.UseVisualStyleBackColor = true;
+            this.btnNewGame.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnRules
+            // 
+            this.btnRules.Font = new System.Drawing.Font("Arial", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRules.Location = new System.Drawing.Point(398, 372);
+            this.btnRules.Name = "btnRules";
+            this.btnRules.Size = new System.Drawing.Size(222, 70);
+            this.btnRules.TabIndex = 1;
+            this.btnRules.Text = "Entrar Partida";
+            this.btnRules.UseVisualStyleBackColor = true;
+            // 
+            // btnExit
+            // 
+            this.btnExit.Font = new System.Drawing.Font("Arial", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExit.Location = new System.Drawing.Point(398, 470);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(222, 70);
+            this.btnExit.TabIndex = 2;
+            this.btnExit.Text = "SAIR";
+            this.btnExit.UseVisualStyleBackColor = true;
+            // 
+            // MagicTrick
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.BackgroundImage = global::MagicTrickGame.Properties.Resources.bg;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1034, 611);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnRules);
+            this.Controls.Add(this.btnNewGame);
+            this.DoubleBuffered = true;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.MaximumSize = new System.Drawing.Size(1050, 650);
+            this.MinimumSize = new System.Drawing.Size(1050, 650);
+            this.Name = "MagicTrick";
+            this.Text = "MagicTrick";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnNewGame;
+        private System.Windows.Forms.Button btnRules;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
+
---
+++ MagicTrickGame/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MagicTrickServer;
+
+namespace MagicTrickGame
+{
+    public partial class MagicTrick : Form
+    {
+        public MagicTrick()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Jogo.
+        }
+    }
+}
